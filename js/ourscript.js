3627e82c68245a7ca850bf248b80dca4e302a8f8
kluangkhflemingcollegec <kluangkh@flemingcollege.ca>
2020-11-22 19:05:44 -0500

    Add Comment

--- js/ourscript.js
+++ js/ourscript.js
@@ -1,5 +1,5 @@
 /*	Created by Kristine Luangkhot on November 22, 2020 */
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/* Show all menu items when the user clicks on the icon, collapse when user clicks on the icon again */
 function navBarMin() {
     var x = document.getElementById("myTopnav");
     if (x.className === "navbar") {
