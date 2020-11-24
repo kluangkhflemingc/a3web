1afe2be6fc5047bb56cff9d78e42d1c4e0abf029
kluangkhflemingcollegec <kluangkh@flemingcollege.ca>
2020-11-24 13:06:34 -0500

    Edit Comments

--- js/ourscript.js
+++ js/ourscript.js
@@ -1,4 +1,5 @@
-/*	Created by Kristine Luangkhot on November 22, 2020 */
+/*	Owner: Kristine Luangkhot on November 22, 2020 */
+/* Copied from: https://www.w3schools.com/howto/howto_js_topnav_responsive.asp */
 /* Show all menu items when the user clicks on the icon, collapse when user clicks on the icon again */
 function navBarMin() {
     var x = document.getElementById("myTopnav");
@@ -8,3 +9,4 @@ function navBarMin() {
       x.className = "navbar";
     }
   }
+/* End Owner: Kristine Luangkhot */
