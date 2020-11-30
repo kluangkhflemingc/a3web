03ba7c702d1906b75471c4b133b5153d31d7bfdc
kluangkhflemingcollegec <kluangkh@flemingcollege.ca>
2020-11-30 12:26:10 -0500

    Change image source test

--- js/ourscript.js
+++ js/ourscript.js
@@ -1,4 +1,7 @@
-/*	Owner: Kristine Luangkhot on November 22, 2020 */
+/*********** 
+ Owner: Kristine Luangkhot
+ Entry Date: November 22, 2020 
+ ***********/
 /* Copied from: https://www.w3schools.com/howto/howto_js_topnav_responsive.asp */
 /* Show all menu items when the user clicks on the icon, collapse when user clicks on the icon again */
 function navBarMin() {
@@ -10,3 +13,17 @@ function navBarMin() {
     }
   }
 /* End Owner: Kristine Luangkhot */
+
+/***********
+Owner: Kristine Luangkhot
+Creation Date: November 30, 2020
+***********/
+
+/* Change image source after 10 seconds */
+setTimeout(function() {
+  document.getElementById("monochrome").setAttribute("src", "images/cartography/kristine-colour-qualitative.jpg");
+}, 10000);
+
+/**********
+ End Owner: Kristine Luangkhot
+ **********/
